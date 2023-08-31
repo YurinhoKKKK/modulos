f6b8b59e208bcce3b139971d474bf283db94edf8
Yuri Mateus Franco <yurimateusfranco@gmail.com>
2023-08-31 08:49:12 -0300

    Segunda Versão

--- calculadora.js
+++ calculadora.js
@@ -3,7 +3,7 @@ var media = require("./modulos/media");
 var dividir = require("./modulos/dividir");
 var multiplicar = require("./modulos/multiplicar");
 
-console.log(somar(20,90));
-console.log(media(20,90));
-console.log(multiplicar (20,90));
-console.log(dividir(20,90));
+console.log("A soma de x e y é: " + somar(20,90));
+console.log("A média de x e y é: " + media(20,90));
+console.log("O produto de x e y é: " + multiplicar (20,90));
+console.log("A divisão de x e y é: " + dividir(20,90));
